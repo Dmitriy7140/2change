029e3dee1a9d0bc91a37a2bfa80d3469d8c572a3
Dmitriy7140 <duked189@gmail.com>
2026-01-14 21:33:52 +0300

    beta 1.1

--- bot_main.py
+++ bot_main.py
@@ -249,16 +249,14 @@ def handle_queue(message):
     last_name = message.from_user.last_name or ""
     user_name = (message.from_user.first_name or "") + (" " + last_name if last_name else "")
 
-    amount2= None
+
 
     lines= qdb.get_from_queue()
     if lines:
 
-        _, tg_id, country, client_name, amount, currency1, currency2, reason, created_at = lines
-        if amount and currency1 and currency2:
+        _, tg_id, country, client_name, amount1, amount2, currency1, currency2, reason, created_at = lines
 
-            amount2= "formula"
-        apmake = ApplicationCreator(country=country, client_name=client_name, amount1=amount, amount2=amount2,currency1=currency1, currency2=currency2, reason=reason, time=created_at)
+        apmake = ApplicationCreator(country=country, client_name=client_name, amount1=amount1,amount2=amount2, currency1=currency1, currency2=currency2, reason=reason, time=created_at)
         msg = apmake.create()
         msg += "\n" + f"\n✅<b>Взят в работу:</b>\n<i>{datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")}</i>\n\n💼Менеджер: {user_name} " + "\n" + f"\n➡️Cсылка на чат с клиентом:<a href='tg://user?id={tg_id}'>➡️ {client_name}</a>"
         msg += "\n" + f"\n⚡️Заявок в очереди: {qdb.count_rows()}"
